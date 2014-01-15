7519567115f729ec28a6c3b6510001a98d092768
Sven Nicolai Viig <sven.viig@dsafe.no>
2014-01-15 10:38:00 +0100

    Better documentation

--- js/app.js
+++ js/app.js
@@ -3,11 +3,27 @@ console.log('Starting app.js');
 var options = {
 	height:200,
 	width:200,
-	type:'area',
+	type:'bar',
 	labels:['jan','feb','mar','apr','may','jun','jul','aug'],
 	data:[.90,.20,.40,.35,.37,.50,.50,.50]
 }
 var chart = SnapChart('#bar-chart',options);
+var options = {
+	height:200,
+	width:200,
+	type:'line',
+	labels:['jan','feb','mar','apr','may','jun','jul','aug'],
+	data:[.90,.20,.40,.35,.37,.50,.50,.50]
+}
+var chart = SnapChart('#line-chart',options);
+var options = {
+	height:200,
+	width:200,
+	type:'area',
+	labels:['jan','feb','mar','apr','may','jun','jul','aug'],
+	data:[.90,.20,.40,.35,.37,.50,.50,.50]
+}
+var chart = SnapChart('#area-chart',options);
 
 // reloading site on focus so i don't have to press cmd-r
 window.addEventListener("focus", function(event) {

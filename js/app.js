630c2aa2cf5c9a8782c85d0e18fe4d05d5a8c9a5
Sven Nicolai Viig <sven.viig@dsafe.no>
2014-01-22 21:51:44 +0100

    updated docs

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ var options = {
 	data:[{
 			values: [.90,.20,.40,.35,.37,.50,.50,.50],
 			color: 'rgba(0,175,255,.5)'
-		},
-		{
-			values: [.10,.25,.6,.7,.4,.9,.7,.8],
-			color: 'rgba(0,0,100,.5)'
 		}
 	]
 };
@@ -40,11 +36,7 @@ var options = {
 		{
 			values: [.90,.20,.40,.35,.37,.50,.50,.50],
 			color: 'rgba(0,175,255,.5)'
-		},
-		{
-			values: [.10,.25,.6,.7,.4,.9,.7,.8],
-			color: 'rgba(0,0,100,.5)'
-		}	
+		}
 	]
 };
 var chart = SnapChart('#area-chart',options);
